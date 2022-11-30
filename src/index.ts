6ebd767f3a1fcaf6d067366d70509bc4541fe586
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2022-11-30 10:37:26 +0000

    save

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
 import { Observable } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-
-// https://random-data-api.com/
-const URL = 'https://random-data-api.com/api/name/random_name'
-const api$ = ajax<any>(URL);
-
-
-api$.subscribe({
-  next: (x) => console.log('next1', x.response.first_name)
-})
-
-api$.subscribe({
-  next: (x) => console.log('next2', x.response.first_name)
-})
-
-api$.subscribe({
-  next: (x) => console.log('next3', x.response.first_name)
-})
